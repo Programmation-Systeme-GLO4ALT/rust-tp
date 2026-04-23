523b267e2b3d1b66a4edff208a38e68370537d15
andreaDarcel <darcelkameni@gmail.com>
2026-04-23 22:28:40 +0100

    .

--- MBONO MASSA EMMANUEL PRINCE/tp2_taches/target/debug/deps/tp2_taches-2a1904453ee79e85.d
+++
@@ -1,5 +0,0 @@
-C:\Users\GLC\Downloads\MBONO MASSA EMMANUEL PRINCE\tp2_taches\target\debug\deps\tp2_taches-2a1904453ee79e85.d: src\main.rs
-
-C:\Users\GLC\Downloads\MBONO MASSA EMMANUEL PRINCE\tp2_taches\target\debug\deps\libtp2_taches-2a1904453ee79e85.rmeta: src\main.rs
-
-src\main.rs:
--- kamdjo_felix_junior/tp2-gestion_memoire/src/main.rs
+++
@@ -1,9 +0,0 @@
-mod partie_a;
-mod partie_b;
-
-fn main() {
-    partie_a::programme1();
-    partie_a::programme2();
-
-    partie_b::run();
-}
